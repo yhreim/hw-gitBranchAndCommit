90acee9e6555fb3f26a889b72dfc66785fde04e5
Teng Zhang <zhangt2333@gmail.com>
2021-05-03 00:55:29 +0800

    hw1: bugfix

--- main.py
+++ main.py
@@ -11,15 +11,13 @@ from bs4 import BeautifulSoup
 username = ''
 
 def hw1():
-    response = requests.get('ttps://raw.githubusercontent.com/{}/hw-gitBranchAndCommit/theLooooooooooongestBranch/1plus1.txt')
+    response = requests.get('https://raw.githubusercontent.com/{}/hw-gitBranchAndCommit/theLooooooooooongestBranch/1plus1.txt'.format(username))
     sys.exit(0 if '1+1=2' in response.text.strip().replace(' ', '') else -1)
 
 methods = [hw1]
 if __name__ == '__main__':
     f = open('in.txt', 'r', encoding='utf-8')
     argv = f.read()
-    print(argv)
-    print(re.findall(r'I finished homework: `(.*?)`', argv))
     hw_index = int(re.findall(r'I finished homework: `(.*?)`', argv)[0])
     username = re.findall(r'`https://github.com/(.*?)/hw-gitBranchAndCommit`', argv)[0]
     methods[hw_index - 1]()
